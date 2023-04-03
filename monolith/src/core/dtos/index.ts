e475ad944d71afcbe8e45f2b3e58ed544803ff5b
NathanRodet <rodetnathan.contact@gmail.com>
2023-04-03 15:09:59 +0200

    Added services

--- monolith/src/core/dtos/index.ts
+++
@@ -1,1 +0,0 @@
-export * from '../dtos/user.dto';
--- monolith/src/services/data-services/data-services.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostgresqlDataServicesModule } from '../../frameworks/data-services/typeorm/typeorm-data.module';
-
-@Module({
-  imports: [PostgresqlDataServicesModule],
-  exports: [PostgresqlDataServicesModule],
-})
-
-export class DataServicesModule { }
