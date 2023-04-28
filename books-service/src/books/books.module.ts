c985bd9bf6039a618a6830fd4f114bfc6f4e5519
Nathan Rodet <rodetnathan.contact@gmail.com>
2023-04-28 14:49:43 +0200

    deleted artefacts

--- books-service/src/books/books.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BooksController } from './books.controller';
-import { BooksService } from './books.service';
-
-@Module({
-  controllers: [BooksController],
-  providers: [BooksService]
-})
-export class BooksModule {}
